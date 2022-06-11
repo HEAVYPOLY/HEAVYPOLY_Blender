8915c58c492f75ca15c9ae1cb6bf8bac9111bcac
Vaughan Ling <kakapoopie@Vaughans-MacBook-Pro.local>
2022-06-11 12:27:42 -0700

    fix border select for and 3.3

--- scripts/startup/HEAVYPOLY_select_through_border.py
+++ scripts/startup/HEAVYPOLY_select_through_border.py
@@ -19,52 +19,53 @@ from bpy.props import IntProperty, BoolProperty
 class HP_OT_select_through_border(bpy.types.Operator):
 	bl_idname = "view3d.select_through_border"
 	bl_label = "Select Through Border"
+	def execute(self, context):
+		print("EXECUTE")
 
 	def modal(self, context, event):
-		bpy.context.space_data.shading.show_xray = True
-		if event.type == 'MOUSEMOVE' and event.value == 'PRESS':
-			bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='SET',wait_for_input=False)
-			return {'RUNNING_MODAL'}
-		if event.type in {'RIGHTMOUSE', 'ESC'}:  # Cancel
+		if event.type == "MOUSEMOVE":
 			bpy.context.space_data.shading.show_xray = False
 			return {'CANCELLED'}
-		elif event.type == 'MOUSEMOVE' and event.value == 'RELEASE':
+
+		elif event.type == 'LEFTMOUSE' and event.value == 'RELEASE':
 			bpy.context.space_data.shading.show_xray = False
+			print("LEFTMOUSE RELEASE")
+			return {'CANCELLED'}
+		elif event.type in {'RIGHTMOUSE', 'ESC'}:  # Cancel
+			bpy.context.space_data.shading.show_xray = False
+			print("CANCELLED")
 			return {'CANCELLED'}
 		return {'RUNNING_MODAL'}
 	def invoke(self, context, event):
-		if bpy.context.space_data.shading.show_xray == True:
-			bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='SET',wait_for_input=False)
-		else:
-			context.window_manager.modal_handler_add(self)
+		bpy.context.space_data.shading.show_xray = True
+		print("INVOKE !")
+		context.window_manager.modal_handler_add(self)
+		bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='SET',wait_for_input=False)
 		return {'RUNNING_MODAL'}
-   # [("view3d.select_box",
-     # {"type": 'LEFTMOUSE', "value": 'CLICK_DRAG'},
-     # {"properties":
-      # [("wait_for_input", True),
-       # ],
+
 class HP_OT_select_through_border_add(bpy.types.Operator):
 	bl_idname = "view3d.select_through_border_add"
 	bl_label = "Select Through Border"
 
 	def modal(self, context, event):
-
-		bpy.context.space_data.shading.show_xray = True
-		if event.type == 'MOUSEMOVE' and event.value == 'PRESS':
-			bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='ADD',wait_for_input=False)
-			return {'RUNNING_MODAL'}
-		if event.type in {'RIGHTMOUSE', 'ESC'}:  # Cancel
+		if event.type == "MOUSEMOVE":
 			bpy.context.space_data.shading.show_xray = False
 			return {'CANCELLED'}
-		elif event.type == 'MOUSEMOVE' and event.value == 'RELEASE':
+
+		elif event.type == 'LEFTMOUSE' and event.value == 'RELEASE':
 			bpy.context.space_data.shading.show_xray = False
+			print("LEFTMOUSE RELEASE")
+			return {'CANCELLED'}
+		elif event.type in {'RIGHTMOUSE', 'ESC'}:  # Cancel
+			bpy.context.space_data.shading.show_xray = False
+			print("CANCELLED")
 			return {'CANCELLED'}
 		return {'RUNNING_MODAL'}
 	def invoke(self, context, event):
-		if bpy.context.space_data.shading.show_xray == True:
-			bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='ADD',wait_for_input=False)
-		else:
-			context.window_manager.modal_handler_add(self)
+		bpy.context.space_data.shading.show_xray = True
+		print("INVOKE !")
+		context.window_manager.modal_handler_add(self)
+		bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='ADD',wait_for_input=False)
 		return {'RUNNING_MODAL'}
 
 class HP_OT_select_through_border_sub(bpy.types.Operator):
@@ -72,22 +73,24 @@ class HP_OT_select_through_border_sub(bpy.types.Operator):
 	bl_label = "Select Through Border"
 
 	def modal(self, context, event):
-		bpy.context.space_data.shading.show_xray = True
-		if event.type == 'MOUSEMOVE' and event.value == 'PRESS':
-			bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='SUB',wait_for_input=False)
-			return {'RUNNING_MODAL'}
-		if event.type in {'RIGHTMOUSE', 'ESC'}:  # Cancel
+		if event.type == "MOUSEMOVE":
 			bpy.context.space_data.shading.show_xray = False
 			return {'CANCELLED'}
-		elif event.type == 'MOUSEMOVE' and event.value == 'RELEASE':
+
+		elif event.type == 'LEFTMOUSE' and event.value == 'RELEASE':
 			bpy.context.space_data.shading.show_xray = False
+			print("LEFTMOUSE RELEASE")
+			return {'CANCELLED'}
+		elif event.type in {'RIGHTMOUSE', 'ESC'}:  # Cancel
+			bpy.context.space_data.shading.show_xray = False
+			print("CANCELLED")
 			return {'CANCELLED'}
 		return {'RUNNING_MODAL'}
 	def invoke(self, context, event):
-		if bpy.context.space_data.shading.show_xray == True:
-			bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='SUB',wait_for_input=False)
-		else:
-			context.window_manager.modal_handler_add(self)
+		bpy.context.space_data.shading.show_xray = True
+		print("INVOKE !")
+		context.window_manager.modal_handler_add(self)
+		bpy.ops.view3d.select_box('INVOKE_DEFAULT',mode='SUB',wait_for_input=False)
 		return {'RUNNING_MODAL'}
 
 classes = (
